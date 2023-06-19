f53c256d7a99882d3a28fb9fbc4fcb43aaa8232c
Vivek <vivekbhagat1890@hotmail.com>
2023-06-19 17:51:15 +0530

    Post the request from the user input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import Postlist from './components/Postlist';
+import PostForm from './components/PostForm';
+// import Postlist from './components/Postlist';
 
 function App() {
   return (
     <div className="App">
-     <Postlist/>
+     {/* <Postlist/> */}
+     <PostForm/>
     </div>
   );
 }
