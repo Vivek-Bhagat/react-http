36528bfb1e3c3a0030427814f82d212636a95e5e
Vivek <vivekbhagat1890@hotmail.com>
2023-06-30 13:53:51 +0530

    comment

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -5,7 +5,7 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
-
+// dark mode toggler is not working right now . Need to work on this later
 class DarkMode extends Component {
   render() {
     return (
