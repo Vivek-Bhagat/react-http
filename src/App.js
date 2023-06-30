ec79a72a0ea53fd4f915a3b3f5e9469f421864ae
Vivek <vivekbhagat1890@hotmail.com>
2023-06-30 12:14:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // import logo from './logo.svg';
-import './App.css';
-import ClickCounter from './components/ClickCounter';
+import "./App.css";
+
+import ClickCounter from "./components/ClickCounter";
 // import PostForm from './components/PostForm';
 // import Postlist from './components/Postlist';
 
 function App() {
   return (
     <div className="App">
-     {/* <Postlist/> */}
-     {/* <PostForm/> */}
-     <ClickCounter/>
+      {/* <Postlist/> */}
+      {/* <PostForm/> */}
+      <ClickCounter />
     </div>
   );
 }
